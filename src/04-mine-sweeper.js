f288c2135f12412b599a002d06dfe4cffead36c9
taccianka <tacciankastudy@gmail.com>
2021-04-09 00:05:26 +0300

    feat: solve task-4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,48 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  let count = 0;
+  const mines = Array.from(
+    Array(matrix.length),
+    () => new Array(matrix[0].length),
+  );
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i >= 1 && j >= 1 && matrix[i - 1][j - 1] === true) {
+        count++;
+      }
+      if (i >= 1 && j < matrix[i].length - 1 && matrix[i - 1][j] === true) {
+        count++;
+      }
+      if (i >= 1 && j < matrix[i].length - 1 && matrix[i - 1][j + 1] === true) {
+        count++;
+      }
+      if (j >= 1 && matrix[i][j - 1] === true) {
+        count++;
+      }
+      if (j < matrix[i].length - 1 && matrix[i][j + 1] === true) {
+        count++;
+      }
+      if (i < matrix.length - 1 && j >= 1 && matrix[i + 1][j - 1] === true) {
+        count++;
+      }
+      if (i < matrix.length - 1 && matrix[i + 1][j] === true) {
+        count++;
+      }
+      if (
+        i < matrix.length - 1
+        && j < matrix[i].length - 1
+        && matrix[i + 1][j + 1] === true
+      ) {
+        count++;
+      }
+      mines[i][j] = count;
+      count = 0;
+    }
+  }
+  return mines;
 }
 
 module.exports = minesweeper;
