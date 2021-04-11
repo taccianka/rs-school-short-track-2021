01391f04f2129a84f342dd7b3277ab23a6e7cac7
taccianka <tacciankastudy@gmail.com>
2021-04-12 01:25:26 +0300

    feat: solve task-7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,19 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const newNames = names.slice();
+  let suffix = 0;
+  for (let i = 0; i < names.length; i++) {
+    for (let j = i + 1; j < names.length; j++) {
+      if (newNames[i].localeCompare(names[j]) === 0) {
+        suffix++;
+        newNames[j] = newNames[j].concat(`(${suffix})`);
+      }
+    }
+    suffix = 0;
+  }
+  return newNames;
 }
 
 module.exports = renameFiles;
